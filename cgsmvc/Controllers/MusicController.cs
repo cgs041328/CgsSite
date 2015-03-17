6e0cdb62fc98603798711e633494edebdd569571
cgs041328 <124592174@qq.com>
2015-03-17 09:08:24 +0800

    我的个人网站
    将用到 bootstrap，asp.net mvc5,EF6,

---
+++ cgsmvc/Controllers/MusicController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cgsmvc.Controllers
+{
+    public class MusicController : Controller
+    {
+        // GET: Musics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ cgsmvc/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace cgsmvc.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+        [Display(Name = "Release Date")]
+        //[DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        [Range(1, 1000)]
+        [DataType(DataType.Currency)]
+        public Decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string Rating { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
