715a91d40b72426233b1e7cdf4ffba340ca2177c
cgs <cgs931219@outlook.com>
2015-06-30 22:27:05 +0800

    2015.6.30
    unittest rename

--- Cgssite.UnitTest/PagingTest.cs
+++ Cgssite.UnitTest/PagingTest.cs
@@ -10,13 +10,19 @@ namespace CgsSite.UnitTest
     public class PagingTest
     {
         [TestMethod]
-        public void testPaging()
+        public void Paging_Constructor_IsNextPageRight()
         {
             Paging page1 = new Paging(100);
             Paging page2 = new Paging(100, 10);
-            Paging page3 = new Paging(101);
+            
             Assert.AreEqual(2, page1.NextPage);
             Assert.AreEqual(-1, page2.NextPage);
+            
+        }
+        [TestMethod]
+        public void Paging_Constructor_IsTotalPageRight()
+        {
+            Paging page3 = new Paging(101);
             Assert.AreEqual(11, page3.TotalPage);
         }
     }
